242abd9887c87b90f9377fd1a3ce78742a085a4d
smaness1 <56251129+smaness1@users.noreply.github.com>
2019-10-08 00:25:10 -0700

    Create AddingMachine.java
    This is the base AddingMachine class that Ms.Calliss gave us but with Javadoc comments

---
+++ AddingMachine.java
@@ -0,0 +1,58 @@
+ *  	This is the AddingMachine class. In this current version, all we have in the class
+ *  	is a private integer, called total, a default constructor that sets its total 
+ *  	attribute to 0 upon creation, and 5 empty methods that are to be filled in later.
+ * 	@param	total	This integer will hold the total of all the additions and subtractions
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+	private int total;
+	private int total;	// private integer that will hold the total
+
+/**
+*	AddingMachine Constructor
+*	this initializes the total to 0
+*/
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+
+
+/**	
+*	getTotal method
+*	@return 	for now, it returns 0
+*/
+	public int getTotal () {
+		return 0;
+	}
+
+/**
+*	add method, this currently does nothing.
+*/
+	public void add (int value) {
+
+	}
+
+
+/**
+*	subtract method, this currently does nothing
+*/
+	public void subtract (int value) {
+
+	}
+
+/**
+*	toString method
+*	@return		for now, it returns an empty string
+*/ 
+	public String toString () {
+		return "";	
+	}
+
+/**
+*	clear method, this method currently does nothing
+*/
+	public void clear() {
+
+	}
